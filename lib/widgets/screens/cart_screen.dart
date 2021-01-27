caffceb1fcab2b04d35a9e026dbbbb34714bc611
Javier Heisecke <javierheiseckee@gmail.com>
2021-01-26 23:15:49 -0300

    Agrega items por carrito

--- lib/widgets/screens/cart_screen.dart
+++ lib/widgets/screens/cart_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../providers/cart.dart';
+import '../../providers/cart.dart' show Cart;
+import '../items/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -44,6 +45,20 @@ class CartScreen extends StatelessWidget {
               ),
             ),
           ),
+          SizedBox(
+            height: 20,
+          ),
+          //with expanded it takes as much space from the column
+          Expanded(
+              child: ListView.builder(
+            itemCount: cart.itemCount,
+            itemBuilder: (context, i) => CartItem(
+              id: cart.items.values.toList()[i].id,
+              title: cart.items.values.toList()[i].title,
+              quantity: cart.items.values.toList()[i].quantity,
+              price: cart.items.values.toList()[i].price,
+            ),
+          )),
         ],
       ),
     );
